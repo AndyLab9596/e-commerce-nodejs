8ad0ea4cb2842d2ff4c1c40206e275bf008eeb14
vyct1 <thienvy95@gmail.com>
2025-06-10 14:50:46 +0700

    init project

---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require("./src/app");
+const PORT = 3055;
+
+const server = app.listen(PORT, () => {
+  console.log(`WSV e-commerce with port ${PORT}`);
+});
+
+process.on("SIGINT", () => {
+  server.close(() => console.log("\nExit Server Express"));
+});
